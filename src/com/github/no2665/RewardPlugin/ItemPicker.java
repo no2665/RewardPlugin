e424506e816d00f9a425da5d6209db0da2fe855f
no2665 <lewischun93@hotmail.com>
2012-06-13 19:45:25 +0100

    Added a few things

--- src/com/github/no2665/RewardPlugin/ItemPicker.java
+++ src/com/github/no2665/RewardPlugin/ItemPicker.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
-import com.gmail.nossr50.datatypes.PlayerProfile;
+import com.gmail.nossr50.util.Users;
 
 public class ItemPicker {
-	public static ItemStack[] pickItem(final Player player, RewardPlugin plugin){
+	public static void pickItem(final Player player, RewardPlugin plugin){
 		final Configuration config = plugin.getConfig();
 		
 		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
 			public void run(){
-				
 				String selectedRewards = "";
-				int powerLevel = (new PlayerProfile(player.getName(), false)).getPowerLevel();
-				
+				int powerLevel = (Users.getProfile(player).getPowerLevel());
 				for(Map<?, ?> m : config.getMapList("Rewards")){
 					//Check each level set in the config file to see if it is greater than
 					//the players power level, and if it is, stop checking
@@ -30,14 +29,17 @@ public class ItemPicker {
 				
 				Scanner scanItems = new Scanner(selectedRewards);
 				while(scanItems.hasNext()){
-					int itemType = scanItems.nextInt();
-					int quantity = scanItems.nextInt();
-					//Probably shouldn't directly add items here,
-					//Afterall this method does return and itemstack array
-					player.getInventory().addItem(new ItemStack(itemType, quantity));
+					String itemType = scanItems.next();
+					int quantity = 1;
+					if(scanItems.hasNext()) quantity = scanItems.nextInt();
+					if(itemType.contains(":")){
+						int itemID = Integer.parseInt(itemType.substring(0, itemType.indexOf(":")));
+						int itemData = Integer.parseInt(itemType.substring(itemType.indexOf(":") + 1, itemType.length()));
+						player.getInventory().addItem(new MaterialData(itemID, (byte) itemData).toItemStack(quantity));
+					}
+					else player.getInventory().addItem(new ItemStack(Integer.parseInt(itemType), quantity));
 				}
 			}
 		}, 20L);
-		return null;
 	}
 }
--- src/com/github/no2665/RewardPlugin/RewardPlugin.java
+++ src/com/github/no2665/RewardPlugin/RewardPlugin.java
@@ -6,44 +6,56 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RewardPlugin extends JavaPlugin implements Listener{
 	
 	/**
-	 * Bugs I can think of:
-	 * 	-Secondary data that is used for some blocks, i.e. red wool (35:14), not be an int, 
-	 * 		so need a test to see if there is secondary data, in the ItemPicker class.
-	 *
 	 * Improvements:
-	 *  -Config file can set time between rewards
-	 *  -Config can set whether or not the plugin is enabled
-	 *  -Config should set a message given to players when they receive rewards
 	 *  -Add a way of adding money as a reward
-	 *  -Edit config file through commands
+	 *  -Also enchantments
 	 */
 	
-	Map<String, Long> validJoins;
+	private Map<String, Long> validJoins;
 	
-	//Everything starts here
 	@Override
-	public void onEnable(){ 
+	public void onLoad(){
 		File file = new File("plugins" + File.separator + "RewardPlugin" + File.separator + "config.yml");
 		if(!file.exists()){
 			saveDefaultConfig();
 		}
+		checkConfigFile();
+	}
+	
+	private void checkConfigFile(){
+		Scanner scan;
+		for(Map<?, ?> m : getConfig().getMapList("Rewards")){
+			String r = m.get("rewards").toString();
+			scan = new Scanner(r);
+			while(scan.hasNext()){
+				scan.next();
+				if(!scan.hasNext()){
+					getLogger().info("Your configuration file for the Rewards Plugin is missing a quantity for the level " + m.get("level").toString() + " rewards.");
+					getLogger().info("This quantity will be assumed to be 1.");
+				}
+				else scan.nextInt();
+			}
+		}
+	}
+	
+	//Everything starts here
+	@Override
+	public void onEnable(){ 
 		validJoins = loadMap("plugins" + File.separator + "RewardPlugin" + File.separator + "RewardData.bin");
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
@@ -57,27 +69,31 @@ public class RewardPlugin extends JavaPlugin implements Listener{
 	
 	@EventHandler
 	public void onPlayerLogin(PlayerLoginEvent event){
-		final Player player = event.getPlayer();
-		
-		//Create new data for new players
-		if(!validJoins.containsKey(player.getName()))
-			validJoins.put(player.getName(), System.currentTimeMillis());
-		
-		long lastValidJoin = validJoins.get(player.getName());
-		//86400000 for 24 hours
-		if(System.currentTimeMillis() - lastValidJoin >= 10000){
-			//Reset timer, and reward player
-			validJoins.put(player.getName(), System.currentTimeMillis());
-			ItemPicker.pickItem(player, this);
-		}
-		
-		//Send the player a friendly message.
-		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			public void run(){
-				player.sendMessage("Here are your rewards for today");
+		if(getConfig().getBoolean("Enabled")){
+			final Player player = event.getPlayer();
+			
+			//Create new data for new players
+			if(!validJoins.containsKey(player.getName())){
+				validJoins.put(player.getName(), System.currentTimeMillis());
+				ItemPicker.pickItem(player, this);
 			}
-		}, 20L);
-		
+			else{
+				long lastValidJoin = validJoins.get(player.getName());
+				//86400000 for 24 hours
+				if(System.currentTimeMillis() - lastValidJoin >= getConfig().getLong("TimeBetweenRewards")){
+					//Reset timer, and reward player
+					validJoins.put(player.getName(), System.currentTimeMillis());
+					ItemPicker.pickItem(player, this);
+				}
+			}
+			
+			//Send the player a friendly message.
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				public void run(){
+					player.sendMessage(getConfig().getString("RewardMessage"));
+				}
+			}, 20L);
+		}
 	}
 	
 	public void saveMap(Map<String, Long> validJoinsMap, String path)
@@ -104,22 +120,60 @@ public class RewardPlugin extends JavaPlugin implements Listener{
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(command.getName().equals("echo")){
-			if(args.length != 0){
-				if(sender instanceof Player){
-					String echoMessage = "";
-					for(String string: args){
-						echoMessage += string + " ";
-					}
-					sender.sendMessage("Here is your echo: " + echoMessage);
+		if(command.getName().equals("deleteReward")){
+			if(sender.hasPermission("editConfigPerms")){
+				if(args.length == 0) return false;
+				boolean found = false;
+				int level = Integer.parseInt(args[0]);
+				int index = 0;
+				List<Map<?, ?>> configList = getConfig().getMapList("Rewards");
+				for(Map<?, ?> m : configList){
+					if(Integer.parseInt(m.get("level").toString()) < level) index++;	
+					else if(Integer.parseInt(m.get("level").toString()) == level){ found = true; break; }
+					else break;
+				}
+				if(found){
+					configList.remove(index);
+					getConfig().set("Rewards", configList);
+					saveConfig();
+				}
+				else sender.sendMessage("Level " + level + " is not included in the config file.");
+			}
+			else sender.sendMessage("You do not have permission to use this command"); 
+		}
+		else if(command.getName().equals("addReward")){
+			if(sender.hasPermission("editConfigPerms")){
+				boolean edit = false;
+				if(args.length == 0) return false;
+				int level = Integer.parseInt(args[0]);
+				int index = 0;
+				String rewards = "";
+				for(int i = 1; i < args.length; i++){
+					rewards += args[i] + " ";
+				}
+				List<Map<?, ?>> configList = getConfig().getMapList("Rewards");
+				for(Map<?, ?> m : configList){
+					if(Integer.parseInt(m.get("level").toString()) < level) index++;	
+					else if(Integer.parseInt(m.get("level").toString()) == level){ edit = true; break; }
+					else break;
+				}
+				if(edit){
+					@SuppressWarnings("unchecked")
+					Map<String, Object> map = (Map<String, Object>) configList.get(index);
+					map.put("rewards", map.get("rewards") + " " + rewards);
+					configList.set(index, map);
 				}
 				else{
-					sender.sendMessage("You do not need to use this command, you fool!");
+					Map<String, Object> map = new HashMap<String, Object>();
+					map.put("level", level);
+					map.put("rewards", rewards);
+					configList.add(index, map);
 				}
-				return true;
+				getConfig().set("Rewards", configList);
+				saveConfig();
 			}
-			return false;
+			else sender.sendMessage("You do not have permission to use this command"); 
 		}
-		return false;
+		return true;
 	}
 }
